1b9c56e17b5166480be48e0baa5a5ebc6273cef6
Camilo Almeida <camiloalmeida22@gmail.com>
2024-12-03 15:20:21 -0300

    Add ApiConsumption class to service package

---
+++ src/main/java/com/systemnegro/liter_alura/service/ApiConsumption.java
@@ -0,0 +1,28 @@
+package com.systemnegro.liter_alura.service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ApiConsumption {
+    public String consumption() {
+        HttpResponse<String> response;
+        try (HttpClient client = HttpClient.newHttpClient()) {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(""))
+                    .build();
+            try {
+                response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return response.body();
+    }
+
+}
