3a444b0f13537e54b44340b26af1613d846e9261
Camilo Almeida <camiloalmeida22@gmail.com>
2024-12-03 14:58:38 -0300

    first commit

---
+++ src/main/java/com/systemnegro/liter_alura/LiterAluraApplication.java
@@ -0,0 +1,14 @@
+package com.systemnegro.liter_alura;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterAluraApplication  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiterAluraApplication.class, args);
+	}
+
+}
