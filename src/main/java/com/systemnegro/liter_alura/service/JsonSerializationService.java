a9d1775b6dd2318eb4d0d3ae79a9ee68c757f612
Camilo Almeida <camiloalmeida22@gmail.com>
2024-12-04 17:24:45 -0300

    add try-catch block to handle JSON processing exceptions

--- src/main/java/com/systemnegro/liter_alura/service/JsonSerializationService.java
+++ src/main/java/com/systemnegro/liter_alura/service/JsonSerializationService.java
@@ -6,8 +6,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class JsonSerializationService {
     private final ObjectMapper mapper = new ObjectMapper();
 
-    public <T> T fromJson(String json, Class<T> clazz) throws JsonProcessingException {
-        return mapper.readValue(json,clazz);
+    public <T> T fromJson(String json, Class<T> clazz){
+        try {
+            return mapper.readValue(json,clazz);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
