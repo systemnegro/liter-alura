cf8a2c404a6d8c33d13ce340dccd5f9a5cc0606f
Camilo Almeida <camiloalmeida22@gmail.com>
2024-12-06 14:17:59 -0300

    Add queries for fetching books and authors in AuthorRepository

--- src/main/java/com/systemnegro/liter_alura/repository/AuthorRepository.java
+++ src/main/java/com/systemnegro/liter_alura/repository/AuthorRepository.java
@@ -1,7 +1,19 @@
 package com.systemnegro.liter_alura.repository;
 
 import com.systemnegro.liter_alura.model.Author;
+import com.systemnegro.liter_alura.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+    @Query("select b from Author a join a.books b")
+    List<Book> findAllBook();
+
+    @Query("select b from Author a JOIN a.books b WHERE b.title ILIKE %:title%")
+    Optional<Book> findByTitle(String title);
+
+    Optional<Author> findByNameContainingIgnoreCase(String name);
 }
